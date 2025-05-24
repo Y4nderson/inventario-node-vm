84e14692f04a32cdfed0ae7cef8d9ef84fe8ba09
Yanderson <yayomauricio@gmail.com>
2025-05-24 15:57:06 -0600

    Actualizacion con el resto de apis

--- apiMysqlP.js
+++ apiMysqlP.js
@@ -181,6 +181,68 @@ app.put("/api/productosSp", (req, res) => {
     );
 });
 
+//DELETE
+
+// DELETE
+app.delete("/api/productosSp", (req, res) => {
+    const {
+       proceso,
+        productoID,
+        categoriaID,
+        subCategoriaID,
+        nombre,
+        precioCompra,
+        precioVenta,
+        stock,
+        estado,
+        fechaVencimiento
+    } = req.query;
+
+    
+    if (
+        proceso === undefined ||
+        categoriaID === undefined ||
+        subCategoriaID === undefined ||
+        nombre === undefined ||
+        precioCompra === undefined ||
+        precioVenta === undefined ||
+        stock === undefined ||
+        estado === undefined ||
+        fechaVencimiento === undefined
+
+      
+    ) {
+        console.error("DELETE → Faltan parámetros");
+        return res.status(400).json({ error: "Faltan parámetros" });
+    }
+
+    const params = [
+             parseInt(proceso),
+            parseInt(productoID),
+            parseInt(categoriaID),
+            parseInt(subCategoriaID),
+            nombre || '',
+            parseFloat(precioCompra),
+            parseFloat(precioVenta),
+            parseInt(stock),
+            parseInt(estado),
+            fechaVencimiento 
+
+    ];
+
+    console.log("DELETE → Parámetros enviados al SP:", params);
+
+    const sqlQuery = "CALL SP_Producto(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(sqlQuery, params, (err, result) => {
+        if (err) {
+            console.error("Error en el SP:", err);
+            return res.status(500).send("Error en el SP");
+        }
+
+        res.json(result[1][0]);
+    });
+});
 
 // CATEGORIAS
 // GET
@@ -344,6 +406,355 @@ app.delete("/api/categoriasSp", (req, res) => {
     });
 });
 
+//SUBCATEGORIAS
+// GET
+app.get("/api/subCategoriasSp", (req, res) => {
+    const {
+        proceso,
+        subCategoriaID,
+        categoriaID,
+        nombre,
+        estado,
+        usuarioID
+    } = req.query;
+
+    const sqlQuery = "CALL SP_SUBCATEGORIA(?, ?, ?, ?, ?, ?, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(
+        sqlQuery,
+        [
+            parseInt(proceso),
+            parseInt(subCategoriaID),
+            parseInt(categoriaID),
+            nombre || '',
+            parseInt(estado),
+            parseInt(usuarioID)
+        ],
+        (err, result) => {
+            if (err) {
+                console.error("Error en el SP:", err);
+                return res.status(500).send("Error en el SP");
+            }
+
+            res.json(result[0]);
+        }
+    );
+});
+// POST
+app.post("/api/subCategoriasSp", (req, res) => {
+    const {
+        proceso,
+        subCategoriaID,
+        categoriaID,
+        nombre,
+        estado,
+        usuarioID
+    } = req.body;
+
+    if (
+        proceso === undefined ||
+        subCategoriaID === undefined ||
+        categoriaID === undefined ||
+        nombre === undefined ||
+        estado === undefined ||
+        usuarioID === undefined
+    ) {
+        console.error("POST → Faltan parámetros");
+        return res.status(400).json({ error: "Faltan parámetros" });
+    }
+
+    const params = [
+        parseInt(proceso),
+        parseInt(subCategoriaID),
+        parseInt(categoriaID),
+        nombre || '',
+        parseInt(estado),
+        parseInt(usuarioID)
+    ];
+
+    const sqlQuery = "CALL SP_SUBCATEGORIA(?, ?, ?, ?, ?, ?, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(sqlQuery, params, (err, result) => {
+        if (err) {
+            console.error("Error en el SP:", err.sqlMessage || err);
+            return res.status(500).send("Error en el SP");
+        }
+
+        res.json(result[1][0]);
+    });
+});
+
+
+
+
+// PUT
+app.put("/api/subCategoriasSp", (req, res) => {
+    const {
+        proceso,
+        subCategoriaID,
+        categoriaID,
+        nombre,
+        estado,
+        usuarioID
+    } = req.body;
+
+    if (
+        proceso === undefined ||
+        subCategoriaID === undefined ||
+        categoriaID === undefined ||
+        nombre === undefined ||
+        estado === undefined ||
+        usuarioID === undefined
+    ) {
+        console.error("PUT → Faltan parámetros");
+        return res.status(400).json({ error: "Faltan parámetros" });
+    }
+
+    const params = [
+        parseInt(proceso),
+        parseInt(subCategoriaID),
+        parseInt(categoriaID),
+        nombre || '',
+        parseInt(estado),
+        parseInt(usuarioID)
+    ];
+
+    const sqlQuery = "CALL SP_SUBCATEGORIA(?, ?, ?, ?, ?, ?, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(sqlQuery, params, (err, result) => {
+        if (err) {
+            console.error("Error en el SP:", err.sqlMessage || err);
+            return res.status(500).send("Error en el SP");
+        }
+
+        res.json(result[1][0]);
+    });
+});
+
+
+// DELETE
+app.delete("/api/subCategoriasSp", (req, res) => {
+    const {
+        proceso,
+        subCategoriaID,
+        categoriaID,
+        nombre,
+        estado,
+        usuarioID
+    } = req.query;
+
+    if (
+        proceso === undefined ||
+        subCategoriaID === undefined ||
+        categoriaID === undefined ||
+        nombre === undefined ||
+        estado === undefined ||
+        usuarioID === undefined
+    ) {
+        console.error("DELETE → Faltan parámetros");
+        return res.status(400).json({ error: "Faltan parámetros" });
+    }
+
+    const params = [
+        parseInt(proceso),
+        parseInt(subCategoriaID),
+        parseInt(categoriaID),
+        nombre || '',
+        parseInt(estado),
+        parseInt(usuarioID)
+    ];
+
+    const sqlQuery = "CALL SP_SUBCATEGORIA(?, ?, ?, ?, ?, ?, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(sqlQuery, params, (err, result) => {
+        if (err) {
+            console.error("Error en el SP:", err.sqlMessage || err);
+            return res.status(500).send("Error en el SP");
+        }
+
+        res.json(result[1][0]);
+    });
+});
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+//USUARIOS
+app.get("/api/usuariosSp", (req, res) => {
+    const {
+        proceso,
+        usuarioID,
+        username,
+        contrasenaTexto,
+        correoElectronico,
+        nombreCompleto,
+        rol,
+        permisos,
+        estado
+    } = req.query;
+
+    const sqlQuery = "CALL SP_Usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(
+        sqlQuery,
+        [
+            parseInt(proceso),
+            parseInt(usuarioID),
+            username || '',
+            contrasenaTexto || '',
+            correoElectronico || '',
+            nombreCompleto || '',
+            rol || '',
+            permisos || '',
+            parseInt(estado)
+        ],
+        (err, result) => {
+            if (err) {
+                console.error("Error en el SP:", err);
+                return res.status(500).send("Error en el SP");
+            }
+
+            res.json(result[0]);
+        }
+    );
+});
+
+
+app.post("/api/usuariosSp", (req, res) => {
+    const {
+        proceso,
+        usuarioID,
+        username,
+        contrasenaTexto,
+        correoElectronico,
+        nombreCompleto,
+        rol,
+        permisos,
+        estado
+    } = req.body;
+
+    if (
+        proceso === undefined ||
+        usuarioID === undefined ||
+        username === undefined ||
+        contrasenaTexto === undefined ||
+        correoElectronico === undefined ||
+        nombreCompleto === undefined ||
+        rol === undefined ||
+        permisos === undefined ||
+        estado === undefined
+    ) {
+        console.error("POST → Faltan parámetros");
+        return res.status(400).json({ error: "Faltan parámetros" });
+    }
+
+    const params = [
+        parseInt(proceso),
+        parseInt(usuarioID),
+        username || '',
+        contrasenaTexto || '',
+        correoElectronico || '',
+        nombreCompleto || '',
+        rol || '',
+        permisos || '',
+        parseInt(estado)
+    ];
+
+    const sqlQuery = "CALL SP_Usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(sqlQuery, params, (err, result) => {
+        if (err) {
+            console.error("Error en el SP:", err.sqlMessage || err);
+            return res.status(500).send("Error en el SP");
+        }
+
+        res.json(result[1][0]);
+    });
+});
+
+
+app.put("/api/usuariosSp", (req, res) => {
+    const {
+        proceso,
+        usuarioID,
+        username,
+        correoElectronico,
+        nombreCompleto,
+        rol,
+        permisos,
+        estado
+    } = req.body;
+
+    if (
+        proceso === undefined ||
+        usuarioID === undefined ||
+        username === undefined ||
+        correoElectronico === undefined ||
+        nombreCompleto === undefined ||
+        rol === undefined ||
+        permisos === undefined ||
+        estado === undefined
+    ) {
+        console.error("PUT → Faltan parámetros");
+        return res.status(400).json({ error: "Faltan parámetros" });
+    }
+
+    const sqlQuery = "CALL SP_Usuario(?, ?, ?, '', ?, ?, ?, ?, ?, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(
+        sqlQuery,
+        [
+            parseInt(proceso),
+            parseInt(usuarioID),
+            username || '',
+            correoElectronico || '',
+            nombreCompleto || '',
+            rol || '',
+            permisos || '',
+            parseInt(estado)
+        ],
+        (err, result) => {
+            if (err) {
+                console.error("Error en el SP:", err.sqlMessage || err);
+                return res.status(500).send("Error en el SP");
+            }
+
+            res.json(result[1][0]);
+        }
+    );
+});
+
+
+app.delete("/api/usuariosSp", (req, res) => {
+    const {
+        proceso,
+        usuarioID
+    } = req.query;
+
+    if (
+        proceso === undefined ||
+        usuarioID === undefined
+    ) {
+        console.error("DELETE → Faltan parámetros");
+        return res.status(400).json({ error: "Faltan parámetros" });
+    }
+
+    const sqlQuery = "CALL SP_Usuario(?, ?, '', '', '', '', '', '', 0, @respuesta); SELECT @respuesta as respuesta";
+
+    db.query(
+        sqlQuery,
+        [
+            parseInt(proceso),
+            parseInt(usuarioID)
+        ],
+        (err, result) => {
+            if (err) {
+                console.error("Error en el SP:", err.sqlMessage || err);
+                return res.status(500).send("Error en el SP");
+            }
+
+            res.json(result[1][0]);
+        }
+    );
+});
+
 
 
 //MOVIMIENTOS
@@ -493,6 +904,7 @@ const sqlQuery = "CALL SP_MOVIMIENTOS(?, ?, ?, ?,?,?,?, @respuesta); SELECT @res
 //------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
-app.listen(4200, () => {
+app.listen(4200, '0.0.0.0', () => {
+
     console.log("Escuchando por el puerto 4200");
 });
